41cf7934e828d8ad277fd46cb9891dd67b40207c
albertoacn <alberto@tokbox.com>
2017-12-14 21:23:33 -0800

    Initial commit

---
+++ survey_data.js
@@ -0,0 +1,19 @@
+// Hard coded input questions
+module.exports = [
+    {
+        text: 'Hey there!',
+        type: 'text'
+    },
+    {
+        text: 'Have you ever jump-kicked a lemur?',
+        type: 'text'
+    },
+    {
+        text: 'Who is your favorite Teenage Mutant Ninja Turtle and why?',
+        type: 'text'
+    },
+    {
+        text: 'What is you location?\n In iOS please tap information then share your location.\n In Android please tap attach then your location.',
+        type: 'text'
+    }
+];
